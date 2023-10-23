527ce4200174f79c766c4f49a87436e486af4850
GuilhermeRocha0 <gui.rocha009@gmail.com>
2023-10-23 13:20:59 -0300

    feat: add custom login and logout

--- src/main/java/br/com/fiap/library/book/BookController.java
+++ src/main/java/br/com/fiap/library/book/BookController.java
@@ -1,6 +1,8 @@
 package br.com.fiap.library.book;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,7 +22,9 @@ public class BookController {
     BookService service;
 
     @GetMapping
-    public String index(Model model) {
+    public String index(Model model, @AuthenticationPrincipal OAuth2User user) {
+        model.addAttribute("username", user.getAttribute("name"));
+        model.addAttribute("avatar_url", user.getAttribute("avatar_url"));
         model.addAttribute("books", service.findAll());
         return "book/index";
     }
